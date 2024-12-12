be860a488e5386bd3bde59c1d1f15ade729c29dd
noname0310 <hjnam2014@gmail.com>
2024-12-12 11:47:35 +0900

    add chunk send method

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 #include <string>
 #include <chrono>
+#include <cstdint>
+#include <functional>
 #include <wiringPi.h>
 
-constexpr int INPUT_PIN = 22;
-constexpr int OUTPUT_PIN = 22;
+constexpr int32_t INPUT_PIN = 22;
+constexpr int32_t OUTPUT_PIN = 22;
 
-constexpr int ONE_SECOND_IN_MICROSECONDS = 1000000;
+constexpr int32_t ONE_SECOND_IN_MICROSECONDS = 1000000;
 
-constexpr int STOP_BITS = 1;
+constexpr int32_t STOP_BITS = 1;
 
-constexpr int SEND_SIGNAL_TRUE = HIGH;
-constexpr int SEND_SIGNAL_FALSE = LOW;
+constexpr int32_t SEND_SIGNAL_TRUE = HIGH;
+constexpr int32_t SEND_SIGNAL_FALSE = LOW;
 
-constexpr int RECEIVE_SIGNAL_TRUE = LOW;
-constexpr int RECEIVE_SIGNAL_FALSE = HIGH;
+constexpr int32_t RECEIVE_SIGNAL_TRUE = LOW;
+constexpr int32_t RECEIVE_SIGNAL_FALSE = HIGH;
 
 //         __data__
 // 0000000110101010000110101010000
@@ -27,22 +29,26 @@ void busyWait(std::chrono::system_clock::time_point end) {
 }
 
 class SerialSender {
-    int delay;
+    int32_t delay;
 
 public:
-    SerialSender(int baudrate = 9) : delay(ONE_SECOND_IN_MICROSECONDS / baudrate) {
+    SerialSender(int32_t baudrate = 9) : delay(ONE_SECOND_IN_MICROSECONDS / baudrate) {
         pinMode(OUTPUT_PIN, OUTPUT);
         digitalWrite(OUTPUT_PIN, SEND_SIGNAL_FALSE);
     }
 
     void send(const std::string& message) {
-        for (char c : message) {
+        for (int8_t c : message) {
             sendByte(c);
         }
     }
 
+    void fastSend(const std::string& message) {
+        sendBytesAsChunk(message.c_str(), message.size());
+    }
+
 private:
-    void sendByte(char byte) {
+    void sendByte(int8_t byte) {
         auto start = std::chrono::high_resolution_clock::now();
         auto next = start;
 
@@ -52,7 +58,7 @@ private:
         busyWait(next);
 
         // Data bits
-        for (int i = 0; i < 8; ++i) {
+        for (int32_t i = 0; i < 8; ++i) {
             bool bit = (byte >> i) & 0x01;
             digitalWrite(OUTPUT_PIN, bit ? SEND_SIGNAL_TRUE : SEND_SIGNAL_FALSE);
             next += std::chrono::microseconds(delay);
@@ -64,21 +70,81 @@ private:
         next += std::chrono::microseconds(delay * STOP_BITS);
         busyWait(next);
     }
+
+    void sendBytes(const int8_t* bytes, uint8_t size) {
+        auto start = std::chrono::high_resolution_clock::now();
+        auto next = start;
+        
+        // Start bit
+        digitalWrite(OUTPUT_PIN, SEND_SIGNAL_TRUE);
+        next += std::chrono::microseconds(delay);
+        busyWait(next);
+
+        // Data bits
+        {
+            // Data length
+            for (int32_t i = 0; i < 8; ++i) {
+                bool bit = (size >> i) & 0x01;
+                digitalWrite(OUTPUT_PIN, bit ? SEND_SIGNAL_TRUE : SEND_SIGNAL_FALSE);
+                next += std::chrono::microseconds(delay);
+                busyWait(next);
+            }
+
+            // Data
+            for (int32_t i = 0; i < size; ++i) {
+                for (int32_t j = 0; j < 8; ++j) {
+                    bool bit = (bytes[i] >> j) & 0x01;
+                    digitalWrite(OUTPUT_PIN, bit ? SEND_SIGNAL_TRUE : SEND_SIGNAL_FALSE);
+                    next += std::chrono::microseconds(delay);
+                    busyWait(next);
+                }
+            }
+        }
+
+        // Stop bits
+        digitalWrite(OUTPUT_PIN, SEND_SIGNAL_FALSE);
+        next += std::chrono::microseconds(delay * STOP_BITS);
+        busyWait(next);
+    }
+
+    void sendBytesAsChunk(const int8_t* bytes, uint32_t size) {
+        const int8_t* offset = bytes;
+        while (0 < size) {
+            uint32_t chunk = size < 255 ? size : 255;
+            sendBytes(offset, chunk);
+            offset += chunk;
+            size -= chunk;
+        }
+    }
 };
 
 class SerialReceiver {
-    int delay;
+    int32_t delay;
 
 public:
-    SerialReceiver(int baudrate = 9) : delay(ONE_SECOND_IN_MICROSECONDS / baudrate) {
+    SerialReceiver(int32_t baudrate = 9) : delay(ONE_SECOND_IN_MICROSECONDS / baudrate) {
         pinMode(INPUT_PIN, INPUT);
     }
 
     void receive() {
         for (; ;) {
-            char byte = receiveByte();
+            int8_t byte = receiveByte();
             std::cout << byte;
             std::flush(std::cout);
+        }
+    }
+
+    void fastReceive() {
+        for (; ;) {
+            int8_t bytes[255];
+            uint8_t size = receiveBytes(bytes, [](int8_t byte) {
+                std::cout << byte;
+                std::flush(std::cout);
+            });
+            // for (int32_t i = 0; i < size; ++i) {
+            //     std::cout << bytes[i];
+            // }
+            // std::flush(std::cout);
         }
     }
 
@@ -111,34 +177,91 @@ private:
 
         return byte;
     }
+
+    uint8_t receiveBytes(int8_t* bytes, std::function<void(int8_t)> callback) {
+        // Wait for start bit
+        while (digitalRead(INPUT_PIN) == RECEIVE_SIGNAL_FALSE) {
+            // busy wait
+        }
+
+        auto start = std::chrono::high_resolution_clock::now();
+        auto next = start;
+
+        // Wait Start bit
+        next += std::chrono::microseconds(delay + delay / 3);
+        busyWait(next);
+
+        // Data bits
+        uint8_t size = 0;
+        {
+            // Data length
+            for (int32_t i = 0; i < 8; ++i) {
+                bool bit = digitalRead(INPUT_PIN) == RECEIVE_SIGNAL_TRUE;
+                size |= bit << i;
+                next += std::chrono::microseconds(delay);
+                busyWait(next);
+            }
+
+            // Data
+            for (int32_t i = 0; i < size; ++i) {
+                for (int32_t j = 0; j < 8; ++j) {
+                    bool bit = digitalRead(INPUT_PIN) == RECEIVE_SIGNAL_TRUE;
+                    bytes[i] |= bit << j;
+                    next += std::chrono::microseconds(delay);
+                    busyWait(next);
+                }
+                callback(bytes[i]);
+            }
+        }
+
+        // Stop bit start wait
+        next += std::chrono::microseconds(delay / 2);
+        busyWait(next);
+
+        return size;
+    }
 };
 
-enum class Mode : int {
+enum class Role : int32_t {
     SENDER,
     RECEIVER
 };
 
-int main(int argc, char* argv[]) {
-    // program [mode] [baudrate]
-    Mode mode = argc > 1 ? static_cast<Mode>(std::stoi(argv[1])) : Mode::SENDER;
-    int baudrate = argc > 2 ? std::stoi(argv[2]) : 38;
+enum class Mode : int32_t {
+    PERBYTE,
+    CHUNK,
+}
+
+int32_t main(int32_t argc, int8_t* argv[]) {
+    // program [role] [baudrate] [mode]
+    Role role = argc > 1 ? static_cast<Role>(std::stoi(argv[1])) : Role::SENDER;
+    int32_t baudrate = argc > 2 ? std::stoi(argv[2]) : 38;
+    Mode mode = argc > 3 ? static_cast<Mode>(std::stoi(argv[3])) : Mode::PERBYTE;
 
     if (wiringPiSetup() == -1) {
         return 1;
     }
     std::cout << "Setup wiringPi" << std::endl;
 
-    if (mode == Mode::SENDER) {
+    if (role == Role::SENDER) {
         SerialSender sender(baudrate);
         for (; ;) {
             std::string message;
             std::cout << "Enter message: ";
             std::getline(std::cin, message);
-            sender.send(message + '\n');
+            if (mode == Mode::PERBYTE) {
+                sender.send(message);
+            } else {
+                sender.fastSend(message);
+            }
         }
     } else {
         SerialReceiver receiver(baudrate);
-        receiver.receive();
+        if (mode == Mode::PERBYTE) {
+            receiver.receive();
+        } else {
+            receiver.fastReceive();
+        }
     }
 
     return 0;
